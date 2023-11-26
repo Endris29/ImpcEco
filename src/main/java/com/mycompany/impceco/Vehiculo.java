d43cccb094ad66bcfc1d7c8b774f73e083de2191
ENDRIS <endrissanchez12@gmail.com>
2023-11-26 18:12:51 -0500

    Creación de la Clase Vehiculo

---
+++ src/main/java/com/mycompany/impceco/Vehiculo.java
@@ -0,0 +1,30 @@
+public class Vehiculo implements ImpactoEcologico {
+    private double emisionesProduccion;
+    private String tipoCombustible;
+    private double eficienciaCombustible;
+    private double kilometrajeAnual;
+    private double mantenimiento;
+    private int duracion;
+
+    public Vehiculo(double emisionesProduccion, String tipoCombustible, 
+            double eficienciaCombustible, double kilometrajeAnual, 
+            double mantenimiento, int duracion) {
+        this.emisionesProduccion = emisionesProduccion;
+        this.tipoCombustible = tipoCombustible;
+        this.eficienciaCombustible = eficienciaCombustible;
+        this.kilometrajeAnual = kilometrajeAnual;
+        this.mantenimiento = mantenimiento;
+        this.duracion = duracion;
+    }
+
+    @Override
+    public double calcularImpactoEcologico() {
+        return emisionesProduccion + (eficienciaCombustible * kilometrajeAnual)
+                - mantenimiento + (duracion / eficienciaCombustible);
+    }
+
+    @Override
+    public String getIdentificacion() {
+        return "Vehiculo";
+    }
+}
